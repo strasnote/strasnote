694abc1c770964a5574b0ad5188fe01d313b4c69
Craig <craig@nykindale.com>
2021-02-27 11:03:07 +0000

    Initial solution check-in.

---
+++ Strasnote.Auth.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Strasnote.Auth.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Auth API Running...";
+        }
+    }
+}
---
+++ Strasnote.Encryption/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Strasnote.Encryption
+{
+    public class Class1
+    {
+    }
+}
