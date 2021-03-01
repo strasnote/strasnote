5dbf46dc1c500843f135db531871030d655e9488
Ben Green <ben@bcgdesign.com>
2021-03-01 17:22:39 +0000

    Add logging to APIs (see #3)

--- Auth/Strasnote.Auth.Api/Program.cs
+++ Auth/Strasnote.Auth.Api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Strasnote.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +14,13 @@ namespace Strasnote.Auth.Api
 	{
 		public static void Main(string[] args)
 		{
+			ConsoleLog.Enable();
 			CreateHostBuilder(args).Build().Run();
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
+				.UseLogging()
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.UseStartup<Startup>();
--- Notes/Strasnote.Notes.Api/Startup.cs
+++ Notes/Strasnote.Notes.Api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Strasnote.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,8 @@ namespace Strasnote.Notes.Api
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddLogging(Configuration);
+
 			services.AddControllers();
 		}
 
