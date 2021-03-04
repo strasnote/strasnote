607b59755474168f5cb0e84a2c115ab4ed31a314
Ben Green <ben@bcgdesign.com>
2021-03-04 15:31:55 +0000

    Removing redundant lines from csproj files #31

---
+++ Tests/Tests.Strasnote.Logging/Serilog/SerilogLogger/IsEnabled_Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Strasnote.Logging.Serilog_Tests
+{
+	public class IsEnabled_Tests
+	{
+	}
+}
