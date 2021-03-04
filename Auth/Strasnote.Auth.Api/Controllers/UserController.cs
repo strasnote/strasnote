1409cfa8df89935a466795d46aeb80d7a2752096
Ben Green <ben@bcgdesign.com>
2021-03-04 18:08:22 +0000

    Removing unnecessary usings

--- Auth/Strasnote.Auth.Api/Controllers/UserController.cs
+++ Auth/Strasnote.Auth.Api/Controllers/UserController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Strasnote.Auth.Data;
-using Strasnote.Auth.Data.Abstracts;
 using Strasnote.Auth.Data.Entities;
 using Strasnote.Logging;
 
