f8ddf07b738c3f75c6d7dee31bd48123af119c0b
Craig <craig@nykindale.com>
2021-03-07 12:37:40 +0000

    Added basic JWT endpoint implementation. Fixes #1

---
+++ Auth/Strasnote.Auth/Config/JwtConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Strasnote.Auth.Config
+{
+	public class JwtConfig
+	{
+		/// <summary>
+		/// The JWT secret.
+		/// </summary>
+		[Required]
+		public string Secret { get; set; } = string.Empty;
+
+		/// <summary>
+		/// The default JWT access token expiry time.
+		/// </summary>
+		[Required]
+		public int TokenExpiryMinutes { get; set; }
+
+		/// <summary>
+		/// The default JWT refresh token expiry time.
+		/// </summary>
+		[Required]
+		public int RefreshTokenExpiryMinutes { get; set; }
+
+		/// <summary>
+		/// The domain of the JWT issuer.
+		/// </summary>
+		[Required]
+		public string Issuer { get; set; } = string.Empty;
+
+		/// <summary>
+		/// The audience (domain) of the JWT issuer.
+		/// </summary>
+		[Required]
+		public string Audience { get; set; } = string.Empty;
+	}
+}
---
+++ Auth/Strasnote.Auth/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Strasnote.Auth.Abstracts;
+using Strasnote.Auth.Config;
+
+namespace Strasnote.Auth.Extensions
+{
+	public static class ServiceCollectionExtensions
+	{
+		/// <summary>
+		/// Register the auth services
+		/// </summary>
+		/// <param name="services">IServiceCollection</param>
+		/// <param name="configuration">IConfiguration</param>
+		/// <returns>The <paramref name="services"/> object with the added services.</returns>
+		public static IServiceCollection AddAuthServices(this IServiceCollection services, IConfiguration configuration)
+		{
+			// Services
+			services.AddTransient<IJwtToken, JwtToken>();
+
+			// Config options
+			services.AddOptions<AuthConfig>()
+				.Bind(configuration.GetSection(AuthConfig.AppSettingsSectionName))
+				.ValidateDataAnnotations(); // ToDo: implement validation output/log (startup?): https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/options?view=aspnetcore-5.0#options-validation
+
+			return services;
+		}
+	}
+}
