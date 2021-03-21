d90be0348f9a0b3402db9b6bb7f678e5cea475f4
Ben Green <ben@bcgdesign.com>
2021-03-21 07:53:19 +0000

    Disabling warnings on Fake data connection - see #5

--- Data/Strasnote.Data/Fake/DbConnection.cs
+++ Data/Strasnote.Data/Fake/DbConnection.cs
@@ -9,7 +9,10 @@ namespace Strasnote.Data.Fake
 {
 	public sealed class DbConnection : System.Data.Common.DbConnection
 	{
+#pragma warning disable RCS1079 // Throwing of new NotImplementedException.
+#pragma warning disable CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
 		public override string ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+#pragma warning restore CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
 
 		public override string Database => throw new NotImplementedException();
 
@@ -24,5 +27,6 @@ namespace Strasnote.Data.Fake
 		public override void Open() => throw new NotImplementedException();
 		protected override DbTransaction BeginDbTransaction(IsolationLevel isolationLevel) => throw new NotImplementedException();
 		protected override DbCommand CreateDbCommand() => throw new NotImplementedException();
+#pragma warning restore RCS1079 // Throwing of new NotImplementedException.
 	}
 }
