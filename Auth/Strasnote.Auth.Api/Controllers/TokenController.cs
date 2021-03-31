b7adb4e26c4deb3bd061feeb2d3ce8990733d9a3
Craig N <74492500+cwn-dev@users.noreply.github.com>
2021-03-31 10:03:00 +0100

    V0.2 #2 add tests for auth endpoint (#62)
    * TokenResponse record is now public so we can test. Also changed both records to sealed.
    
    * Reverted back to private for the TokenResponse record in here as it just adds confusion making it public. This is a DTO just for this controller.
    
    * TokenResponse record is now public & renamed to TokenResponseViewModel to prevent confusion between the view model and the JwtTokenIssuer service return.
    
    * Added TokenController GetToken tests
    
    * Renamed Data to Storage in solution
    
    * Removed old project
    
    * Installed NSubstitute

--- Auth/Strasnote.Auth.Api/Controllers/TokenController.cs
+++ Auth/Strasnote.Auth.Api/Controllers/TokenController.cs
@@ -19,8 +19,8 @@ namespace Strasnote.Auth.Api.Controllers
 		private readonly IJwtTokenIssuer jwtTokenIssuer;
 		private readonly ILog<TokenController> log;
 
-		public record TokenRequest([Required] string Email, [Required] string Password);
-		record TokenResponse(string AccessToken, string RefreshToken, string? Message, bool Success);
+		public sealed record TokenRequest([Required] string Email, [Required] string Password);
+		public sealed record TokenResponseViewModel(string AccessToken, string RefreshToken, string? Message, bool Success);
 
 		public TokenController(IJwtTokenIssuer jwtToken, ILog<TokenController> log) =>
 			(this.jwtTokenIssuer, this.log) = (jwtToken, log);
@@ -31,7 +31,7 @@ namespace Strasnote.Auth.Api.Controllers
 			log.Trace("User {@User} logging in", tokenRequest.Email);
 			var token = await jwtTokenIssuer.GetTokenAsync(tokenRequest.Email, tokenRequest.Password);
 
-			var tokenResponse = new TokenResponse(token.AccessToken, token.RefreshToken, token.Message, token.Success);
+			var tokenResponse = new TokenResponseViewModel(token.AccessToken, token.RefreshToken, token.Message, token.Success);
 
 			if (!token.Success)
 			{
---
+++ Tests/Auth/Tests.Strasnote.Auth.Api/GetToken_Tests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using Strasnote.Auth.Abstracts;
+using Strasnote.Auth.Api.Controllers;
+using Strasnote.Auth.Models;
+using Strasnote.Logging;
+using Strasnote.Util;
+using Xunit;
+using static Strasnote.Auth.Api.Controllers.TokenController;
+
+namespace Tests.Strasnote.Auth.Api
+{
+    public sealed class GetToken_Tests
+    {
+		private readonly IJwtTokenIssuer jwtTokenIssuer = Substitute.For<IJwtTokenIssuer>();
+		private readonly ILog<TokenController> log = Substitute.For<ILog<TokenController>>();
+
+		[Fact]
+		public async Task Ok_Response_Returned_On_Successful_Call()
+		{
+			// Arrange
+			var tokenController = new TokenController(
+				jwtTokenIssuer,
+				log);
+
+			var tokenRequest = new TokenRequest(
+				Rnd.Str,
+				Rnd.Str);
+
+			jwtTokenIssuer.GetTokenAsync(Arg.Any<string>(), Arg.Any<string>())
+				.Returns(new TokenResponse(Rnd.Str, true));
+
+			// Act
+			var result = await tokenController.GetToken(tokenRequest);
+			var okResult = result as OkObjectResult;
+
+			// Assert
+			Assert.NotNull(okResult);
+			Assert.Equal((int)HttpStatusCode.OK, okResult?.StatusCode);
+		}
+
+		[Fact]
+		public async Task TokenResponseViewModel_Returned_On_Successful_Call()
+		{
+			// Arrange
+			var tokenController = new TokenController(
+				jwtTokenIssuer,
+				log);
+
+			var tokenRequest = new TokenRequest(
+				Rnd.Str,
+				Rnd.Str);
+
+			var tokenResponse = new TokenResponse(
+				Rnd.Str, 
+				Rnd.Str);
+
+			jwtTokenIssuer.GetTokenAsync(Arg.Any<string>(), Arg.Any<string>())
+				.Returns(tokenResponse);
+
+			// Act
+			var result = await tokenController.GetToken(tokenRequest);
+			var okResult = (OkObjectResult)result;
+
+			// Assert
+			Assert.IsAssignableFrom<TokenResponseViewModel>(okResult.Value);
+		}
+
+		[Fact]
+		public async Task TokenResponseViewModel_Contains_Access_Token_And_Refresh_Token_On_Successful_Call()
+		{
+			// Arrange
+			var tokenController = new TokenController(
+				jwtTokenIssuer,
+				log);
+
+			var tokenRequest = new TokenRequest(
+				Rnd.Str,
+				Rnd.Str);
+
+			var tokenResponse = new TokenResponse(
+				Rnd.Str,
+				Rnd.Str);
+
+			jwtTokenIssuer.GetTokenAsync(Arg.Any<string>(), Arg.Any<string>())
+				.Returns(tokenResponse);
+
+			// Act
+			var result = await tokenController.GetToken(tokenRequest);
+			var okResult = (OkObjectResult)result;
+			var tokenResponseViewModel = (TokenResponseViewModel)okResult.Value;
+
+			// Assert
+			Assert.False(string.IsNullOrWhiteSpace(tokenResponseViewModel.AccessToken));
+			Assert.False(string.IsNullOrWhiteSpace(tokenResponseViewModel.RefreshToken));
+			Assert.True(tokenResponseViewModel.Success);
+		}
+
+		[Fact]
+		public async Task Token_Success_False_Returns_Unauthorized_Response()
+		{
+			// Arrange
+			var tokenController = new TokenController(
+				jwtTokenIssuer,
+				log);
+
+			var tokenRequest = new TokenRequest(
+				Rnd.Str,
+				Rnd.Str);
+
+			jwtTokenIssuer.GetTokenAsync(Arg.Any<string>(), Arg.Any<string>())
+				.Returns(new TokenResponse(Rnd.Str, false));
+
+			// Act
+			var result = await tokenController.GetToken(tokenRequest);
+			var unauthorizedResult = result as UnauthorizedObjectResult;
+
+			// Assert
+			Assert.NotNull(unauthorizedResult);
+			Assert.Equal((int)HttpStatusCode.Unauthorized, unauthorizedResult?.StatusCode);
+		}
+
+		[Fact]
+		public async Task Token_Success_False_Returns_Token_Response_Value()
+		{
+			// Arrange
+			var tokenController = new TokenController(
+				jwtTokenIssuer,
+				log);
+
+			var tokenRequest = new TokenRequest(
+				Rnd.Str,
+				Rnd.Str);
+
+			jwtTokenIssuer.GetTokenAsync(Arg.Any<string>(), Arg.Any<string>())
+				.Returns(new TokenResponse(Rnd.Str, false));
+
+			// Act
+			var result = await tokenController.GetToken(tokenRequest);
+			var unauthorizedResult = (UnauthorizedObjectResult)result;
+			var tokenResponseViewModel = (TokenResponseViewModel)unauthorizedResult.Value;
+
+			// Assert
+			Assert.False(tokenResponseViewModel.Success);
+			Assert.True(string.IsNullOrWhiteSpace(tokenResponseViewModel.AccessToken));
+			Assert.True(string.IsNullOrWhiteSpace(tokenResponseViewModel.RefreshToken));
+		}
+    }
+}
