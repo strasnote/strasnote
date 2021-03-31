6df122547eb4e4110b64b79ba66a03eb1da1f3e6
Craig <craig@nykindale.com>
2021-03-31 10:33:05 +0100

    Added unit tests for FindByIdAsync

---
+++ Tests/Auth/Storage/Tests.Strasnote.Auth.Data/UserStore/Tests_FindByIdAsync.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System.Threading;
+using System.Threading.Tasks;
+using NSubstitute;
+using Strasnote.Auth.Data;
+using Strasnote.Auth.Data.Abstracts;
+using Strasnote.Data.Entities.Auth;
+using Xunit;
+
+namespace Tests.Strasnote.Auth.Data
+{
+	public sealed class Tests_FindByIdAsync
+	{
+		private readonly IUserContext userContext = Substitute.For<IUserContext>();
+		private readonly IRoleContext roleContext = Substitute.For<IRoleContext>();
+
+		public Tests_FindByIdAsync()
+		{
+			userContext.RetrieveByIdAsync<UserEntity>(Arg.Any<long>())
+				.Returns(new UserEntity());
+		}
+
+		[Fact]
+		public async Task UserEntity_Returned_On_Successful_Call()
+		{
+			// Arrange
+			var userStore = new UserStore(userContext, roleContext);
+
+			// Act
+			var result = await userStore.FindByIdAsync("1", new CancellationToken());
+
+			// Assert
+			Assert.IsAssignableFrom<UserEntity>(result);
+		}
+
+		[Fact]
+		public async Task UserContext_RetrieveByIdAsync_Is_Called_Once()
+		{
+			// Arrange
+			var userStore = new UserStore(userContext, roleContext);
+
+			// Act
+			await userStore.FindByIdAsync("1", new CancellationToken());
+
+			// Assert
+			await userContext.Received(1).RetrieveByIdAsync<UserEntity>(Arg.Any<long>());
+		}
+	}
+}
