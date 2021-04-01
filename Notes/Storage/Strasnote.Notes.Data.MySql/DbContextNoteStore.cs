1d6955ac8e28aba7b2d4357dd4dacbfd423b61c6
Ben Green <ben@bcgdesign.com>
2021-04-01 18:18:56 +0100

    Prototyping Note Storage layout - see #11

---
+++ Notes/Storage/Strasnote.Notes.Data.MySql/DbContextNoteStore.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Strasnote.Data.Abstracts;
+using Strasnote.Data.Entities.Notes;
+using Strasnote.Logging;
+using Strasnote.Notes.Storage.Abstracts;
+
+namespace Strasnote.Notes.Storage.DbContext
+{
+	/// <summary>
+	/// Note Storage in a database
+	/// </summary>
+	public sealed class DbContextNoteStore : NoteStore
+	{
+		private readonly IDbContext<NoteEntity> dbContext;
+
+		private readonly ILog log;
+
+		public DbContextNoteStore(IDbContext<NoteEntity> dbContext, ILog<DbContextNoteStore> log) =>
+			(this.dbContext, this.log) = (dbContext, log);
+
+		protected override Task<bool> DoCreateAsync(Note note) => throw new System.NotImplementedException();
+		protected override Task<Note> DoGetAsync(string path) => throw new System.NotImplementedException();
+		protected override Task<IEnumerable<Note>> DoGetAsync(string path, bool recursive) => throw new System.NotImplementedException();
+		protected override Task<bool> DoSaveAsync(Note note) => throw new System.NotImplementedException();
+		protected override Task<bool> DoDeleteAsync(string path) => throw new System.NotImplementedException();
+		protected override Task<bool> DoDeleteAsync(Note note) => throw new System.NotImplementedException();
+	}
+}
