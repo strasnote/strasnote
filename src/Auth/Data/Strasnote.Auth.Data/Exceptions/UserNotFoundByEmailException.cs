02b5b580348e2d7eb9de29944962d22d1fabc3e3
Ben Green <ben@bcgdesign.com>
2021-04-06 08:12:51 +0100

    Adding nullability support to UserStore, throwing exceptions when user not found

---
+++ src/Auth/Data/Strasnote.Auth.Data/Exceptions/UserNotFoundByEmailException.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System;
+
+namespace Strasnote.Auth.Data.Exceptions
+{
+	public sealed class UserNotFoundByEmailException : UserNotFoundException
+	{
+		public UserNotFoundByEmailException() { }
+		public UserNotFoundByEmailException(string email) : base($"Email: {email}") { }
+		public UserNotFoundByEmailException(string message, Exception inner) : base(message, inner) { }
+	}
+}
---
+++ src/Auth/Data/Strasnote.Auth.Data/Exceptions/UserNotFoundByUsernameException.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System;
+
+namespace Strasnote.Auth.Data.Exceptions
+{
+	public sealed class UserNotFoundByUsernameException : UserNotFoundException
+	{
+		public UserNotFoundByUsernameException() { }
+		public UserNotFoundByUsernameException(string username) : base($"Username: {username}") { }
+		public UserNotFoundByUsernameException(string message, Exception inner) : base(message, inner) { }
+	}
+}
