0774210155ab7f142a8ed100eb762e696a89fc2c
Ben Green <ben@bcgdesign.com>
2021-04-09 08:12:50 +0100

    Adding DateTime / UTC tests (#103)
    * Adding test for DateTimeOffset properties - closes #101
    
    * Adding tests to ensure DateTime properties set value to UTC - closes #102
    
    * Moving test DateTime entity to separate namespace - see #101, see #102
    
    * Marking utcDateTime local variable as unused - see #102
    
    * Adding DateTime.Kind check for UTC - closes #102

---
+++ src/Data/Strasnote.Data.Entities/Tests/DateTimeTestEntity.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System;
+using Strasnote.Data.Abstracts;
+
+namespace Strasnote.Data.Entities.Tests
+{
+	/// <summary>
+	/// This entity only exists to ensure that the test for DateTimeOffset properties works correctly
+	/// </summary>
+	internal class DateTimeTestEntity : IEntity
+	{
+		/// <summary>
+		/// Unused ID
+		/// </summary>
+		public long Id { get; init; }
+
+		/// <summary>
+		/// Unused test property - see Tests.Strasnote.Data.Entities.DateTimeOffset_Tests
+		/// </summary>
+		public DateTimeOffset TestOffsetProperty { get; set; }
+
+		/// <summary>
+		/// Unused test property - see Tests.Strasnote.Data.Entities.DateTimeUtc_Tests
+		/// </summary>
+		public DateTime TestUtcProperty { get; set; }
+	}
+}
