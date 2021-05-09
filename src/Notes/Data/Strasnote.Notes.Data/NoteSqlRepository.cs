8dadcf21b7244b029f839349366053c7046c011c
Ben Green <ben@bcgdesign.com>
2021-05-09 15:52:07 +0100

    Adding NoteRepository

---
+++ src/Notes/Data/Strasnote.Notes.Data/NoteSqlRepository.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using Strasnote.Data;
+using Strasnote.Data.Abstracts;
+using Strasnote.Data.Entities.Notes;
+using Strasnote.Logging;
+using Strasnote.Notes.Data.Abstracts;
+
+namespace Strasnote.Notes.Data
+{
+	/// <inheritdoc cref="INoteRepository"/>
+	public sealed class NoteSqlRepository : SqlRepository<NoteEntity>, INoteRepository
+	{
+		/// <summary>
+		/// Inject objects
+		/// </summary>
+		/// <param name="client">ISqlClient</param>
+		/// <param name="log">ILog</param>
+		public NoteSqlRepository(ISqlClient client, ILog<NoteSqlRepository> log) : base(client, log, client.Tables.Note) { }
+	}
+}
