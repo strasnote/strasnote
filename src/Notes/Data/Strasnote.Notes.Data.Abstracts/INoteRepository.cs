4ee21749ce8e64e7bff495f567e8bfa172a6fc8b
Ben Green <ben@bcgdesign.com>
2021-05-09 15:46:35 +0100

    Adding INoteRepository (#128)

---
+++ src/Notes/Data/Strasnote.Notes.Data.Abstracts/INoteRepository.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using Strasnote.Data.Abstracts;
+using Strasnote.Data.Entities.Notes;
+
+namespace Strasnote.Notes.Data.Abstracts
+{
+	/// <summary>
+	/// Abstraction for interacting with the note database
+	/// </summary>
+	public interface INoteRepository : IRepository<NoteEntity>
+	{
+	}
+}
