50bb8be6a995e3c29778ca8613a735b88fc7f4db
Ben Green <ben@bcgdesign.com>
2021-05-09 16:12:15 +0100

    Adding NoteController with GET and POST methods (#130)
    * Adding NoteController with GET and POST methods - see #106
    * Registering INoteRepository to NoteSqlRepository - closes #106

---
+++ src/Notes/Strasnote.Notes.Api/Controllers/NoteController.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Strasnote.Data.Entities.Notes;
+using Strasnote.Notes.Api.Models.Notes;
+using Strasnote.Notes.Data.Abstracts;
+
+namespace Strasnote.Notes.Api.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class NoteController : Controller
+	{
+		private readonly INoteRepository notes;
+
+		public NoteController(INoteRepository notes) =>
+			this.notes = notes;
+
+		[HttpPost]
+		public Task<long> Create(NoteEntity note) =>
+			notes.CreateAsync(note);
+
+		[HttpGet("{id}")]
+		public Task<GetModel?> GetById(long id) =>
+			notes.RetrieveAsync<GetModel>(id);
+	}
+}
---
+++ src/Notes/Strasnote.Notes.Api/Models/Notes/GetModel.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Strasnote.Notes.Api.Models.Notes
+{
+	public sealed class GetModel
+	{
+	}
+}
