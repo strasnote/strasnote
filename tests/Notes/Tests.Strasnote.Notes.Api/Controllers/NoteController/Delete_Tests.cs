e5268d0895ac5260946eac3ecc6f7d9eb2b0858e
Ben Green <ben@bcgdesign.com>
2021-05-25 14:38:32 +0100

    Adding NotesController tests - see #144

---
+++ tests/Notes/Tests.Strasnote.Notes.Api/Controllers/NoteController/Delete_Tests.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System.Threading.Tasks;
+using NSubstitute;
+using Strasnote.Notes.Data.Abstracts;
+using Strasnote.Util;
+using Xunit;
+
+namespace Strasnote.Notes.Api.Controllers.NoteController_Tests
+{
+	public class Delete_Tests
+	{
+		[Fact]
+		public async Task Calls_Notes_DeleteAsync()
+		{
+			// Arrange
+			var notes = Substitute.For<INoteRepository>();
+			var controller = new NoteController(notes);
+			var id = Rnd.Lng;
+
+			// Act
+			await controller.Delete(id).ConfigureAwait(false);
+
+			// Assert
+			await notes.Received().DeleteAsync(id).ConfigureAwait(false);
+		}
+	}
+}
---
+++ tests/Notes/Tests.Strasnote.Notes.Api/Controllers/NoteController/GetById_Tests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System.Threading.Tasks;
+using NSubstitute;
+using Strasnote.Notes.Api.Models.Notes;
+using Strasnote.Notes.Data.Abstracts;
+using Strasnote.Util;
+using Xunit;
+
+namespace Strasnote.Notes.Api.Controllers.NoteController_Tests
+{
+	public class GetById_Tests
+	{
+		[Fact]
+		public async Task Calls_Notes_RetrieveAsync()
+		{
+			// Arrange
+			var notes = Substitute.For<INoteRepository>();
+			var controller = new NoteController(notes);
+			var id = Rnd.Lng;
+
+			// Act
+			await controller.GetById(id).ConfigureAwait(false);
+
+			// Assert
+			await notes.Received().RetrieveAsync<GetModel?>(id).ConfigureAwait(false);
+		}
+	}
+}
