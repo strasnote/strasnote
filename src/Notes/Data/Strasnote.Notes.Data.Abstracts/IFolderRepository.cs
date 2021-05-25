fce634b5d871e56597577560b2a949ac57d1a313
Ben Green <ben@bcgdesign.com>
2021-05-25 15:12:49 +0100

    Adding IFolderRepository and implementation - see #145

---
+++ src/Notes/Data/Strasnote.Notes.Data.Abstracts/IFolderRepository.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using Strasnote.Data.Abstracts;
+using Strasnote.Data.Entities.Notes;
+
+namespace Strasnote.Notes.Data.Abstracts
+{
+	/// <summary>
+	/// Abstraction for interacting with notes in the database
+	/// </summary>
+	public interface IFolderRepository : IRepository<FolderEntity>
+	{
+	}
+}
---
+++ src/Notes/Data/Strasnote.Notes.Data/FolderSqlRepository.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System;
+using System.Threading.Tasks;
+using Strasnote.Data;
+using Strasnote.Data.Abstracts;
+using Strasnote.Data.Entities.Notes;
+using Strasnote.Logging;
+using Strasnote.Notes.Data.Abstracts;
+
+namespace Strasnote.Notes.Data
+{
+	/// <inheritdoc cref="IFolderRepository"/>
+	public sealed class FolderSqlRepository : SqlRepository<FolderEntity>, IFolderRepository
+	{
+		/// <summary>
+		/// Inject objects
+		/// </summary>
+		/// <param name="client">ISqlClient</param>
+		/// <param name="log">ILog</param>
+		public FolderSqlRepository(ISqlClient client, ILog<NoteSqlRepository> log) : base(client, log, client.Tables.Folder) { }
+
+		/// <inheritdoc/>
+		public override Task<long> CreateAsync(FolderEntity entity) =>
+			base.CreateAsync(entity with
+			{
+				FolderCreated = DateTime.Now,
+				FolderUpdated = DateTime.Now
+			});
+	}
+}
