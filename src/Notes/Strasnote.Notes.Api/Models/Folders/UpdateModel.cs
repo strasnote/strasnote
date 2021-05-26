870a8ea1fdd70e0d4c7c90f6f8520e2cc0e09cf9
Craig <craig@nykindale.com>
2021-05-26 19:48:33 +0100

    Added CRUD endpoints for folders

---
+++ src/Notes/Strasnote.Notes.Api/Models/Folders/UpdateModel.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+namespace Strasnote.Notes.Api.Models.Folders
+{
+	public sealed class UpdateModel
+	{
+		public string FolderName { get; init; } = string.Empty;
+	}
+}
