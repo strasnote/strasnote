546d7d5b1709005af0619625b1e52aac49d9e520
Craig <craig@nykindale.com>
2021-05-26 19:52:47 +0100

    Added unit tests for FolderController.

---
+++ tests/Notes/Tests.Strasnote.Notes.Api/Controllers/FolderController/Create_Tests.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System.Threading.Tasks;
+using NSubstitute;
+using Strasnote.Data.Entities.Notes;
+using Strasnote.Notes.Data.Abstracts;
+using Xunit;
+
+namespace Strasnote.Notes.Api.Controllers.FolderController_Tests
+{
+	public class Create_Tests
+	{
+		[Fact]
+		public async Task Calls_Folders_CreateAsync()
+		{
+			// Arrange
+			var folders = Substitute.For<IFolderRepository>();
+			var controller = new FolderController(folders);
+			var folder = new FolderEntity();
+
+			// Act
+			await controller.Create(folder).ConfigureAwait(false);
+
+			// Assert
+			await folders.Received().CreateAsync(folder).ConfigureAwait(false);
+		}
+	}
+}
---
+++ tests/Notes/Tests.Strasnote.Notes.Api/Controllers/FolderController/GetById_Tests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System.Threading.Tasks;
+using NSubstitute;
+using Strasnote.Notes.Api.Models.Folders;
+using Strasnote.Notes.Data.Abstracts;
+using Strasnote.Util;
+using Xunit;
+
+namespace Strasnote.Notes.Api.Controllers.FolderController_Tests
+{
+	public class GetById_Tests
+	{
+		[Fact]
+		public async Task Calls_Folders_RetrieveAsync()
+		{
+			// Arrange
+			var folders = Substitute.For<IFolderRepository>();
+			var controller = new FolderController(folders);
+			var id = Rnd.Lng;
+
+			// Act
+			await controller.GetById(id).ConfigureAwait(false);
+
+			// Assert
+			await folders.Received().RetrieveAsync<GetModel?>(id).ConfigureAwait(false);
+		}
+	}
+}
