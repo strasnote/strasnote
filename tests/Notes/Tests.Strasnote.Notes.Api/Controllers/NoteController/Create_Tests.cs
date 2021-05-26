db3f0916437a08d2e8c7a41da21d880212ef9110
Craig N <74492500+cwn-dev@users.noreply.github.com>
2021-05-26 20:35:33 +0100

    v0.6 #162 note create creates blank note (#172)
    * Note create now creates a blank note
    
    * Added unit test to ensure the FolderId is set when creating a note.
    
    * NoteContent on the GetModel is now nullable.

--- tests/Notes/Tests.Strasnote.Notes.Api/Controllers/NoteController/Create_Tests.cs
+++ tests/Notes/Tests.Strasnote.Notes.Api/Controllers/NoteController/Create_Tests.cs
@@ -4,6 +4,7 @@
 using System.Threading.Tasks;
 using NSubstitute;
 using Strasnote.Data.Entities.Notes;
+using Strasnote.Notes.Api.Models.Notes;
 using Strasnote.Notes.Data.Abstracts;
 using Xunit;
 
@@ -17,13 +18,26 @@ namespace Strasnote.Notes.Api.Controllers.NoteController_Tests
 			// Arrange
 			var notes = Substitute.For<INoteRepository>();
 			var controller = new NoteController(notes);
-			var note = new NoteEntity();
 
 			// Act
-			await controller.Create(note).ConfigureAwait(false);
+			await controller.Create(new CreateModel(1)).ConfigureAwait(false);
 
 			// Assert
-			await notes.Received().CreateAsync(note).ConfigureAwait(false);
+			await notes.Received().CreateAsync(Arg.Any<NoteEntity>()).ConfigureAwait(false);
+		}
+		[Fact]
+		public async Task Sets_FolderId_On_NoteEntity()
+		{
+			// Arrange
+			var notes = Substitute.For<INoteRepository>();
+			var controller = new NoteController(notes);
+			var createNoteModel = new CreateModel(1);
+
+			// Act
+			await controller.Create(createNoteModel).ConfigureAwait(false);
+
+			// Assert
+			await notes.Received().CreateAsync(new NoteEntity { FolderId = createNoteModel.FolderId }).ConfigureAwait(false);
 		}
 	}
 }
