e8f9ef14fe6d406a10d379e6acce63e28d342b30
bfren <ben@bcgdesign.com>
2021-06-30 10:01:02 +0100

    Using a new connection for each query (#185)

--- tests/Data/Tests.Strasnote.Data/_/SqlRepository/Constructor_Tests.cs
+++ tests/Data/Tests.Strasnote.Data/_/SqlRepository/Constructor_Tests.cs
@@ -1,26 +1,12 @@
 ﻿// Copyright (c) Strasnote
 // Licensed under https://strasnote.com/licence
 
-using NSubstitute;
 using Xunit;
 
 namespace Strasnote.Data.SqlRepository_Tests
 {
 	public class Constructor_Tests
 	{
-		[Fact]
-		public void Calls_Client_Connect()
-		{
-			// Arrange
-			var (_, client, _, log, table) = SqlRepository_Setup.Get();
-
-			// Act
-			var _ = new TestRepository(client, log, table);
-
-			// Assert
-			client.Received().Connect();
-		}
-
 		[Fact]
 		public void Sets_Log_Property()
 		{
