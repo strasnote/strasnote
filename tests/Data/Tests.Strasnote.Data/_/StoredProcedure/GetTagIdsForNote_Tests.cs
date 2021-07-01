be722054312dcacbda787ec1baf1b277b900891c
Ben Green <ben@bcgdesign.com>
2021-07-01 10:17:33 +0100

    Adding tests - closes #155

---
+++ tests/Data/Tests.Strasnote.Data/_/StoredProcedure/GetTagIdsForNote_Tests.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using Xunit;
+
+namespace Strasnote.Data.StoredProcedure_Tests
+{
+	public class GetTagIdsForNote_Tests
+	{
+		[Fact]
+		public void Returns_Correct_Procedure_Name()
+		{
+			// Arrange
+			const string expected = "GetTagIdsForNote";
+
+			// Act
+			var result = StoredProcedure.GetTagIdsForNote;
+
+			// Assert
+			Assert.Equal(expected, result);
+		}
+	}
+}
---
+++ tests/Notes/Tests.Strasnote.Notes.Api/Controllers/NoteController/AddTag_Tests.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System.Threading.Tasks;
+using NSubstitute;
+using Strasnote.Util;
+using Xunit;
+
+namespace Strasnote.Notes.Api.Controllers.NoteController_Tests
+{
+	public class AddTag_Tests : NoteController_Tests
+	{
+		[Fact]
+		public async Task Calls_Tags_AddToNote()
+		{
+			// Arrange
+			var (controller, v) = Setup();
+			var noteId = Rnd.Ulng;
+			var tagId = Rnd.Ulng;
+
+			// Act
+			await controller.AddTag(noteId, tagId);
+
+			// Assert
+			await v.Tags.Received().AddToNote(tagId, noteId, v.UserId);
+		}
+	}
+}
