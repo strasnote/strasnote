ee94c843fbce9229e7c0b3d9984b9b12ddfe40c7
Craig N <craig@nykindale.com>
2022-01-16 21:18:46 +0000

    Added a new PUT:/note/1/move endpoint which allows you to remove a note from all folders (set FolderId to null) or move a note to another folder.

---
+++ src/Data/Clients/Strasnote.Data.Clients.MySql/Migrations/0007_Note_FolderId_Nullable.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using SimpleMigrations;
+
+namespace Strasnote.Data.Clients.MySql.Migrations
+{
+	[Migration(7, "main.note.FolderId is now nullable (null = unfiled)")]
+	public sealed class Note_FolderId_Nullable : Migration
+	{
+		protected override void Up() => Execute(@"
+			ALTER TABLE `main.note`
+			CHANGE COLUMN `FolderId` `FolderId` BIGINT(20) NULL DEFAULT NULL AFTER `NoteUpdated`;
+		");
+
+		protected override void Down() => Execute(@"
+			ALTER TABLE `main.note`
+			CHANGE COLUMN `FolderId` `FolderId` BIGINT(20) NOT NULL DEFAULT '0' AFTER `NoteUpdated`;
+		");
+	}
+}
---
+++ src/Notes/Strasnote.Notes.Api/Models/Notes/MoveToFolderModel.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using Strasnote.Notes.Api.Models.Notes;
+
+namespace Strasnote.Notes.Api.Models.Notes
+{
+	/// <summary>
+	/// See <see cref="Controllers.NoteController.MoveToFolder(ulong, MoveToFolderModel)"/>
+	/// </summary>
+	public sealed record MoveToFolderModel
+	{
+		/// <summary>
+		/// See <see cref="Controllers.NoteController.MoveToFolder(ulong, MoveToFolderModel)"/>
+		/// </summary>
+		public ulong? FolderId { get; init; }
+	}
+}
