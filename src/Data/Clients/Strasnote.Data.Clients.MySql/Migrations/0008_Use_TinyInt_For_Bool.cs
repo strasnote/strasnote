9c040081db53d80076f116d516fd7cb5d15a1392
Craig N <74492500+cwn-dev@users.noreply.github.com>
2022-01-17 19:16:11 +0000

    Added migration to set auth.user.LockEnabled to TINYINT instead of BIT as required by MySqlConnector. Fixes #206 (#207)

---
+++ src/Data/Clients/Strasnote.Data.Clients.MySql/Migrations/0008_Use_TinyInt_For_Bool.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using SimpleMigrations;
+
+namespace Strasnote.Data.Clients.MySql.Migrations
+{
+	[Migration(8, "Now using MySqlConnector which maps bit(1) to ulong. We need to use tinyint unsigned instead.")]
+	public sealed class Use_TinyInt_For_Bool : Migration
+	{
+		protected override void Down() => Execute(@"
+			ALTER TABLE `auth.user`
+			CHANGE COLUMN `LockoutEnabled` `LockoutEnabled` TINYINT NOT NULL DEFAULT '0' AFTER `LockoutEnd`;
+		");
+		protected override void Up() => Execute(@"
+			ALTER TABLE `auth.user`
+			CHANGE COLUMN `LockoutEnabled` `LockoutEnabled` BIT NOT NULL DEFAULT 0 AFTER `LockoutEnd`;
+		");
+	}
+}
