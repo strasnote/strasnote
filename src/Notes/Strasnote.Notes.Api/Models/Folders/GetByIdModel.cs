0de6c7f2711751ad658db27210df324ea92c2a58
Ben Green <ben@bcgdesign.com>
2022-02-25 14:51:47 +0000

    Putting properties in alphabetical order - closesl #218

--- src/Notes/Strasnote.Notes.Api/Models/Folders/GetByIdModel.cs
+++ src/Notes/Strasnote.Notes.Api/Models/Folders/GetByIdModel.cs
@@ -8,9 +8,9 @@ namespace Strasnote.Notes.Api.Models.Folders
 	/// <summary>
 	/// See <see cref="Controllers.FolderController.GetById(FolderIdModel)"/>
 	/// </summary>
-	/// <param name="Id">Folder ID</param>
-	/// <param name="FolderName">Folder name</param>
 	/// <param name="FolderCreated">When the folder was created</param>
+	/// <param name="FolderName">Folder name</param>
 	/// <param name="FolderUpdated">When the folder was last updated</param>
-	public sealed record GetByIdModel(ulong Id, string FolderName, DateTime FolderCreated, DateTime FolderUpdated);
+	/// <param name="Id">Folder ID</param>
+	public sealed record GetByIdModel(DateTime FolderCreated, string FolderName, DateTime FolderUpdated, ulong Id);
 }
