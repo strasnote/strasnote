fdead59572e4c08722b2068ea08591edbcb387c4
bfren <ben@bcgdesign.com>
2022-03-16 09:16:01 +0000

    Adding TypeHandler to convert DateTime to UTC before inserting into db - closes #213 (#226)

---
+++ src/Data/Strasnote.Data/TypeHandlers/DateTimeTypeHandler.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+using System;
+using System.Data;
+using Dapper;
+
+namespace Strasnote.Data.TypeHandlers
+{
+	/// <summary>
+	/// Ensure DateTime values are stored as UTC.
+	/// </summary>
+	public class DateTimeTypeHandler : SqlMapper.TypeHandler<DateTime>
+	{
+		/// <summary>
+		/// Store value as UTC.
+		/// </summary>
+		/// <param name="parameter">IDbDataParameter</param>
+		/// <param name="value">DateTime</param>
+		public override void SetValue(IDbDataParameter parameter, DateTime value) =>
+			parameter.Value = value.ToUniversalTime();
+
+		/// <summary>
+		/// Parse value and specify it is UTC.
+		/// </summary>
+		/// <param name="value">Database value</param>
+		/// <exception cref="InvalidCastException">Thrown if <paramref name="value"/> is not a valid <see cref="DateTime"/></exception>
+		public override DateTime Parse(object value) =>
+			DateTime.TryParse(value?.ToString(), out DateTime dt) switch
+			{
+				true =>
+					DateTime.SpecifyKind(dt, DateTimeKind.Utc),
+
+				false =>
+					throw new InvalidCastException($"'{value}' is not a valid DateTime.")
+			};
+	}
+}
