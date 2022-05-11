d9d96d1152f017407317346e09b729c90e022e9b
bfren <ben@bcgdesign.com>
2022-05-11 17:19:20 +0100

    Scaffolding web project - #247

---
+++ src/Strasnote.Apps.Web/Formats.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+namespace Strasnote.Apps.Web;
+
+public static class Formats
+{
+	public static string DateTimeFull =>
+		"yyyy-MM-dd HH:mm:ss.ffff";
+}
