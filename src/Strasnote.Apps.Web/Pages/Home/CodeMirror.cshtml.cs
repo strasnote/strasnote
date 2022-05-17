2956cfd8df4a4014fb42831ac40c6ee4511eaea7
bfren <ben@bcgdesign.com>
2022-05-17 10:36:49 +0100

    Adding CodeMirror test page - #247

---
+++ src/Strasnote.Apps.Web/Pages/Home/CodeMirror.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Strasnote.Apps.Web.Pages.Home
+{
+    public class CodeMirrorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
