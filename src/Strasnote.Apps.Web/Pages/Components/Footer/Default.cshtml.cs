ea7531bc219070dfd36c98037df2e6d223a61517
bfren <bf@bfren.dev>
2022-09-25 11:55:06 +0100

    Adding responsive header, fixed footer, and offcanvas sidebar - #255

--- src/Strasnote.Apps.Web/Pages/Components/Footer/Default.cshtml.cs
+++ src/Strasnote.Apps.Web/Pages/Components/Footer/Default.cshtml.cs
@@ -1,14 +1,21 @@
 // Copyright (c) Strasnote
 // Licensed under https://strasnote.com/licence
 
+using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Strasnote.Apps.Web.Pages.Components.Footer;
 
-public sealed record class FooterModel;
+public sealed record class FooterModel(
+	string Build,
+	DateTime LastModified
+);
 
 public sealed class FooterViewComponent : ViewComponent
 {
 	public IViewComponentResult Invoke() =>
-		View(new FooterModel());
+		View(new FooterModel(
+			Build: "v0.8",
+			LastModified: File.GetLastWriteTime(Assembly.GetExecutingAssembly().Location)
+		));
 }
---
+++ src/Strasnote.Apps.Web/Pages/Components/Sidebar/Menu.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Strasnote
+// Licensed under https://strasnote.com/licence
+
+namespace Strasnote.Apps.Web.Pages.Components.Sidebar;
+
+public sealed record class Menu(
+	string Title,
+	MenuItem[] Items
+);
+
+public sealed record class MenuItem(
+	string Text,
+	string Page
+);
